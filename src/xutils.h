3ff2b5e0cc451f19af118756ce2e134814f43111
Ross Burton <rburton@src.gnome.org>
2005-03-08 11:28:42 +0000

    Copy private code from wnck, as use it

---
+++ src/xutils.h
@@ -0,0 +1,42 @@
+/* Xlib utilities */
+
+/*
+ * Copyright (C) 2001 Havoc Pennington
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+
+#ifndef WNCK_XUTILS_H
+#define WNCK_XUTILS_H
+
+#include <glib.h>
+#include <X11/Xlib.h>
+#include <X11/Xatom.h>
+#include <gdk/gdk.h>
+#include <gdk/gdkx.h>
+
+G_BEGIN_DECLS
+
+Atom my_wnck_atom_get  (const char *atom_name);
+
+void my_wnck_change_state (Screen *screen, Window xwindow, gboolean add, Atom state1, Atom state2);
+
+void my_wnck_error_trap_push (void);
+int my_wnck_error_trap_pop (void);
+
+G_END_DECLS
+
+#endif /* WNCK_XUTILS_H */
